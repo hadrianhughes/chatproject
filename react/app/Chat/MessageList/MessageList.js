0a69411dc44bba7d59b4d36405a314007b1c5dd9
Hadrian Hughes <hhughes@amaze.com>
2017-01-24 15:46:49 +0000

    Changed position of user's name on message.

--- react/app/Chat/MessageList/MessageList.js
+++ react/app/Chat/MessageList/MessageList.js
@@ -48,7 +48,7 @@ export default class MessageList extends React.Component
                 this.props.list[i].value = this.props.list[i].value.replace(exp, '<img class="emoji" src="' + this.props.emojis[j].image + '" />');
             }
             
-            this.props.list[i].text = '<b>' + this.props.list[i].user + '</b>: ' + this.props.list[i].value;
+            this.props.list[i].text = '<b>' + this.props.list[i].user + '</b><br />' + this.props.list[i].value;
         }
         
         const listContent = this.props.list.map((item) => <li key={item.key} style={{ textAlign: item.user == this.props.username ? 'right' : 'left'}} dangerouslySetInnerHTML={{__html: item.text}}></li>);
